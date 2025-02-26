f531f94b03184acb30a202573f807af893ac8221
IngridASilva <ingrid27.silvaraujo@gmail.com>
2025-02-26 08:26:36 -0300

    Conducting basic consultations

--- exercise_bd-sql.py
+++ exercise_bd-sql.py
@@ -6,11 +6,32 @@ cursor = students.cursor()
 # cursor.execute('CREATE TABLE students (id INT, name VARCHAR(100), age INT, course VARCHAR(100));')
 
 # 2)
-cursor.execute('INSERT INTO students(id, name, age, course) VALUES (1, "Ingrid", 24, "Economics")')
-cursor.execute('INSERT INTO students(id, name, age, course) VALUES (2, "Jessica", 18, "Journalism")')
-cursor.execute('INSERT INTO students(id, name, age, course) VALUES (3, "Elisa", 24, "Engineering")')
-cursor.execute('INSERT INTO students(id, name, age, course) VALUES (4, "Thalissa", 28, "Biology")')
-cursor.execute('INSERT INTO students(id, name, age, course) VALUES (5, "Brenda", 19, "Administration")')
+# cursor.execute('INSERT INTO students(id, name, age, course) VALUES (1, "Ingrid", 24, "Economics")')
+# cursor.execute('INSERT INTO students(id, name, age, course) VALUES (2, "Jessica", 18, "Journalism")')
+# cursor.execute('INSERT INTO students(id, name, age, course) VALUES (3, "Elisa", 24, "Engineering")')
+# cursor.execute('INSERT INTO students(id, name, age, course) VALUES (4, "Thalissa", 28, "Biology")')
+# cursor.execute('INSERT INTO students(id, name, age, course) VALUES (5, "Brenda", 19, "Administration")')
+
+# 3)
+# a)
+# all_records = cursor.execute('SELECT * FROM students')
+# for students in all_records:
+    # print(students)
+
+# b)
+# name_age_records = cursor.execute('SELECT name, age FROM students WHERE age > 20')
+# for students in name_age_records:
+    # print(students)
+
+# c)
+# course_records = cursor.execute('SELECT * FROM students WHERE course = "Engineering" ORDER BY name')
+# for students in course_records:
+    # print(students)
+
+# d)
+# count_records = cursor.execute('SELECT COUNT(*) FROM students')
+# count_records = cursor.fetchone()[0]
+    # print(f"Total de alunos: {count_records}")
 
 students.commit()
 students.close()
